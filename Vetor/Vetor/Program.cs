fc7f1c3cb8ed14ed0a16d72b55b3624d07b1fc07
matusilva <matheus10xy@gmail.com>
2019-11-11 14:00:10 -0300

    inserindo TAD Vector

---
+++ Vetor/Vetor/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vetor vetor = new Vetor();
+
+            vetor.insertAtRank(0, "A");
+            Console.WriteLine(vetor.toString());
+
+            vetor.insertAtRank(1, "B");
+            Console.WriteLine(vetor.toString());
+
+            vetor.insertAtRank(0, "A2");
+            Console.WriteLine(vetor.toString());
+
+            //vetor.replaceAtRank(0, "A2");
+            //Console.WriteLine(vetor.toString());
+
+            vetor.removeAtRank(1);
+            Console.WriteLine(vetor.toString());
+
+            //vetor.insertAtRank(1, "A");
+            //Console.WriteLine(vetor.toString());
+
+            //Console.WriteLine(vetor.elemAtRank(1));
+
+            Console.WriteLine("\nImprimindo todo o Vetor");
+            Console.WriteLine(vetor.imprimir());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Vetor/Vetor/Vetor.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vetor
+{
+    class Vetor
+    {
+        private Object[] vetor; //array
+        private int n;          //tamanho do vetor
+        public Vetor()
+        {
+            vetor = new Object[1];
+            this.n = 0;
+        }
+        public int size()
+        {
+            return n;
+        }
+        public bool isEmpty()
+        {
+            return n == 0;
+        }
+        public Object elemAtRank(int rank)
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("Vetor Vazio");
+                return "";
+            }
+            else
+            {
+                return vetor[rank];
+            }
+        }
+        public void insertAtRank(int rank, Object element)
+        {
+            if (this.n == this.vetor.Length)
+            {
+                Object[] novoVetor = new Object[vetor.Length * 2];
+                for (int i = 0; i < n; i++)
+                {
+                    novoVetor[i] = this.vetor[i];
+                }
+                this.vetor = novoVetor;
+            }
+
+            for (int i = n-1; i >= rank; i--)
+            {
+                //this.vetor[i] = this.vetor[i + 1];
+                this.vetor[i + 1] = this.vetor[i];
+            }
+
+            this.vetor[rank] = element;
+            n++;
+        }
+        public Object removeAtRank(int rank)
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("Vetor Vazio");
+                return "";
+            }
+
+            else
+            {
+                Object temp = vetor[rank];
+                for (int i = rank; i < n - 1; i++)
+                {
+                    this.vetor[i] = this.vetor[i + 1];
+                    this.vetor[i + 1] = null;
+                }
+
+                n--;
+                return temp;
+            }
+        }
+        public Object replaceAtRank(int rank, Object element)
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("Vetor Vazio");
+                return "";
+            }
+
+            else
+            {
+                this.vetor[rank] = element;
+                return element;
+            }
+        }
+        public String toString()
+        {
+            //int s = size();
+            string saida = "size:" + n + "\t ";
+
+            for (int i = 0; i < n; i++)
+            {
+                saida += vetor[i] + " ";
+            }
+            return saida;
+        }
+        public String imprimir()
+        {
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Console.Write(vetor[i] + " ");
+            }
+            return " ";
+        }
+    }
+}
